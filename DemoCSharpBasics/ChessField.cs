0e9fa8b6cb53c7b38e1474af6d3976bb11b3d961
nikola.ajdukovic <nikola.ajdukovic@mera.com>
2021-08-31 14:33:42 +0200

    09 lists vs arrays demo part 1

--- DemoCSharpBasics/ChessField.cs
+++ DemoCSharpBasics/ChessField.cs
@@ -173,5 +173,57 @@ namespace DemoCSharpBasics
 
             return chessFields;
         }
+
+        public static ChessField[] CreateChessFieldsWithArray() 
+        {
+            ChessField[] chessFields = new ChessField[64];
+
+            chessFields[0] = new ChessField('A', 1);
+            chessFields[1] = new ChessField('A', 2);
+            chessFields[2] = new ChessField('A', 3);
+            chessFields[3] = new ChessField('A', 4);
+            chessFields[4] = new ChessField('A', 5);
+            chessFields[5] = new ChessField('A', 6);
+            chessFields[6] = new ChessField('A', 7);
+            chessFields[7] = new ChessField('A', 8);
+
+            chessFields[8] = new ChessField('B', 1);
+            chessFields[9] = new ChessField('B', 2);
+            chessFields[10] = new ChessField('B', 3);
+            chessFields[11] = new ChessField('B', 4);
+            chessFields[12] = new ChessField('B', 5);
+            chessFields[13] = new ChessField('B', 6);
+            chessFields[14] = new ChessField('B', 7);
+            chessFields[15] = new ChessField('B', 8);
+
+            chessFields[16] = new ChessField('C', 1);
+            chessFields[17] = new ChessField('C', 2);
+            chessFields[18] = new ChessField('C', 3);
+            chessFields[19] = new ChessField('C', 4);
+            chessFields[20] = new ChessField('C', 5);
+            chessFields[21] = new ChessField('C', 6);
+            chessFields[22] = new ChessField('C', 7);
+            chessFields[23] = new ChessField('C', 8);
+
+            chessFields[24] = new ChessField('D', 1);
+            chessFields[25] = new ChessField('D', 2);
+            chessFields[26] = new ChessField('D', 3);
+            chessFields[27] = new ChessField('D', 4);
+            chessFields[28] = new ChessField('D', 5);
+            chessFields[29] = new ChessField('D', 6);
+            chessFields[30] = new ChessField('D', 7);
+            chessFields[31] = new ChessField('D', 8);
+
+            chessFields[32] = new ChessField('E', 1);
+            chessFields[33] = new ChessField('E', 2);
+            chessFields[34] = new ChessField('E', 3);
+            chessFields[35] = new ChessField('E', 4);
+            chessFields[36] = new ChessField('E', 5);
+            chessFields[37] = new ChessField('E', 6);
+            chessFields[38] = new ChessField('E', 7);
+            chessFields[39] = new ChessField('E', 8);
+
+            return chessFields;
+        }
     }
 }
--- DemoCSharpBasics/Program.cs
+++ DemoCSharpBasics/Program.cs
@@ -27,158 +27,21 @@ namespace DemoCSharpBasics
         }
 
         public static void Main(string[] args)
-        {
-
-            List<Shape> soldShapes = new List<Shape>();
-            ShapeStore shapeStore = new ShapeStore(soldShapes);
-            Sphere plastricSphere = new Sphere(name: "sphere", r: 2, Material.PLASTIC, price: 6);
-            Cube plasticCube = new Cube(name: "cube", a: 2, Material.PLASTIC, price: 8);
-            Cylinder plasticCylinder = new Cylinder(name: "cylinder", r: 2, H: 3, Material.PLASTIC, price: 10);
-            Cone cone = new Cone(name: "cone", r: 2, H: 3);
-
-            //Add()
-            soldShapes.Add(plastricSphere);
-            soldShapes.Add(plasticCube);
-            soldShapes.Add(plasticCylinder);
-            //soldShapes.Add(cone);
-
-            Console.WriteLine("Add()" + System.Environment.NewLine);
-            WriteListItems(soldShapes);
-
-            //AddRange()
-            Sphere woodSphere = new Sphere(name: "sphere", r: 2, Material.WOOD, price: 16);
-            Cube woodCube = new Cube(name: "cube", a: 2, Material.WOOD, price: 18);
-            Cylinder woodCylinder = new Cylinder(name: "cylinder", r: 2, H: 3, Material.WOOD, price: 20);
-            List<Shape> soldWoodShapes = new List<Shape>();
-            soldWoodShapes.Add(woodSphere);
-            soldWoodShapes.Add(woodCube);
-            soldWoodShapes.Add(woodCylinder);
-
-            soldShapes.AddRange(soldWoodShapes);
-
-            Console.WriteLine("AddRange(soldWoodShapes)" + System.Environment.NewLine);
-            WriteListItems(soldShapes);            
-
-            //Insert()
-            Sphere metalSphere = new Sphere(name: "sphere", r: 2, Material.METAL, price: 26);
-            soldShapes.Insert(0, metalSphere);
-
-            Console.WriteLine("Insert(0, metalSphere)" + System.Environment.NewLine);
-            WriteListItems(soldShapes);
-
-            //InsertRange()
-            Cube metalCube = new Cube(name: "cube", a: 2, Material.METAL, price: 30);
-            Cylinder metalCylinder = new Cylinder(name: "cylinder", r: 2, H: 3, Material.METAL, price: 32);
-            List<Shape> soldMetalShapes = new List<Shape>();
-            soldMetalShapes.Add(metalCube);
-            soldMetalShapes.Add(metalCylinder);
-            soldShapes.InsertRange(0, soldMetalShapes);
-
-            Console.WriteLine("InsertRange(0, soldMetalShapes)" + System.Environment.NewLine);
-            WriteListItems(soldShapes);
-
-            //GetRange()
-            List<Shape> firstThreeSoldShapes = soldShapes.GetRange(0, 3);
-
-            Console.WriteLine("firstThreeSoldShapes = GetRange(0, 3)" + System.Environment.NewLine);
-            WriteListItems(firstThreeSoldShapes);
-
-            //Find()
-
-            Shape firstSoldWoodShape = soldShapes.Find(soldShape => soldShape.Material == Material.WOOD);
-            
-            Console.WriteLine("firstSoldWoodShape = soldShapes.Find(soldShape => soldShape.Material == Material.WOOD)" + System.Environment.NewLine);
-            WriteItem(firstSoldWoodShape);
-
-            //FindLast()
-            Shape lastSoldWoodShape = soldShapes.FindLast(soldShape => soldShape.Material == Material.WOOD);
-
-            Console.WriteLine("lastSoldWoodShape = soldShapes.FindLast(soldShape => soldShape.Material == Material.WOOD)" + System.Environment.NewLine);
-            WriteItem(lastSoldWoodShape);
-
-            //FindAll()
-            List<Shape> allSoldWoodShapes = soldShapes.FindAll(soldShape => soldShape.Material == Material.WOOD);
-
-            Console.WriteLine("allSoldWoodShapes = soldShapes.FindAll(soldShape => soldShape.Material == Material.WOOD)" + System.Environment.NewLine);
-            WriteListItems(allSoldWoodShapes);
-
-            //FindIndex()
-            int firstSoldWoodShapeIndexInList = soldShapes.FindIndex(soldShape => soldShape.Material == Material.WOOD);
-
-            Console.WriteLine("First sold wood shape index in list is " + firstSoldWoodShapeIndexInList + System.Environment.NewLine);
-
-            //FindLastIndex()
-            int lastSoldWoodShapeIndexInList = soldShapes.FindLastIndex(soldShape => soldShape.Material == Material.WOOD);
-
-            Console.WriteLine("Last sold wood shape index in list is " + lastSoldWoodShapeIndexInList + System.Environment.NewLine);
-
-            //IndexOf()
-            firstSoldWoodShapeIndexInList = soldShapes.IndexOf(firstSoldWoodShape);
-
-            Console.WriteLine("[soldShapes.IndexOf(firstSoldWoodShape)]");
-            Console.WriteLine("First sold wood shape index in list is " + firstSoldWoodShapeIndexInList + System.Environment.NewLine);
-
-            //Contains()
-            Cube glassCube = new Cube(name: "cube", a: 2, Material.GLASS, price: 60);
-            bool isAnyWoodCubeIsSold = soldShapes.Contains(woodCube);
-
-            Console.WriteLine("[soldShapes.Contains(woodCube)]");
-            Console.WriteLine("Is any wood cube is sold? - " + isAnyWoodCubeIsSold + System.Environment.NewLine);
-
-            bool isAnyGlassCubeIsSold = soldShapes.Contains(glassCube);
-
-            Console.WriteLine("[soldShapes.Contains(glassCube)]");
-            Console.WriteLine("Is any glass cube is sold? - " + isAnyGlassCubeIsSold + System.Environment.NewLine);
-
-            //Exists()
-            if (soldShapes.Exists(soldShape => soldShape.Name.Equals("cube") && soldShape.Material == Material.WOOD) == true) 
-            {
-                Console.WriteLine("[if (soldShapes.Exists(soldShape => soldShape.Name.Equals(\"cube\") && soldShape.Material == Material.WOOD) == true)]");
-                Console.WriteLine("Is any wood cube is sold? - True" + System.Environment.NewLine);
-            }
-
-            if (soldShapes.Exists(soldShape => soldShape.Name.Equals("glass") && soldShape.Material == Material.GLASS) == false)
-            {
-                Console.WriteLine("[if (soldShapes.Exists(soldShape => soldShape.Name.Equals(\"glass\") && soldShape.Material == Material.GLASS) == true)]");
-                Console.WriteLine("Is any glass cube is sold? - False" + System.Environment.NewLine);
-            }
-
-            //Sort()
-            List<double> prices = new List<double> { 6, 8, 10, 16, 18, 20, 60, 26, 30, 32};
-            prices.Sort();
-
-            Console.WriteLine("Prices by ascending order");
-            Console.WriteLine(string.Join(",", prices) + System.Environment.NewLine);
-
-            //Reverse()
-            prices.Reverse();
-            Console.WriteLine("Prices by descending order");
-            Console.WriteLine(string.Join(",", prices) + System.Environment.NewLine);
+        {                    
 
             //AsReadOnly()
             List<ChessField> fields = ChessField.CreateChessFields();
-            ReadOnlyCollection<ChessField> chessFields = fields.AsReadOnly();
+            ReadOnlyCollection<ChessField> chessFieldsList = fields.AsReadOnly();
 
             fields.Add(new ChessField('J',2));
-            //chessFields.Add(new ChessField('J', 2));
+            //chessFieldsList.Add(new ChessField('J', 2));
 
-            //TrueForAll()
-            double priceOfPlacticCone = 3.5;
+            ChessField[] chessFieldsArray = ChessField.CreateChessFieldsWithArray();
 
-            if (soldShapes.TrueForAll(soldShape => soldShape.Price > priceOfPlacticCone )) 
-            {
-                Console.WriteLine("Are all sold shapes more expensive than plastic cone? - True." + System.Environment.NewLine);
-            }
+            ChessField listItem = fields[4];
 
-            //TrimExcess()
-            List<Shape> expectedSoldShapes = new List<Shape>(100);
-            expectedSoldShapes.AddRange(soldShapes);
+            ChessField arrayItem = chessFieldsArray[4];
 
-            Console.WriteLine("Capacity of expectedSoldShapes before TrimExcess is " + expectedSoldShapes.Capacity + System.Environment.NewLine);
-
-            expectedSoldShapes.TrimExcess();
-
-            Console.WriteLine("Capacity of expectedSoldShapes after TrimExcess is " + expectedSoldShapes.Capacity + System.Environment.NewLine);
 
             Console.ReadKey();
         }
