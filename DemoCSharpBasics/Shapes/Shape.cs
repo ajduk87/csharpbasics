f786f4439682fd79a9cbd1294d9a7d8d43296f08
nikola.ajdukovic <nikola.ajdukovic@mera.com>
2021-10-06 17:00:09 +0200

    23 method overloading demo 1 update 2

--- DemoCSharpBasics/Shapes/Shape.cs
+++ DemoCSharpBasics/Shapes/Shape.cs
@@ -32,6 +32,7 @@ namespace DemoCSharpBasics.Shapes
         //order item processing first way - delivery at the order amount at once
         public OrderItem ProcessOrderItemDeliveryAtOnce(OrderItem orderItem, int orderAmount)
         {
+            orderItem.DeliveryWay = "at once";
             orderItem.OrderAmount = orderAmount;
 
             if (orderItem.OrderAmount > 20)
@@ -54,6 +55,7 @@ namespace DemoCSharpBasics.Shapes
         //order item processing second way - delivery at the order amount in two parts
         public OrderItem ProcessOrderItemDeliveryInTwoParts(OrderItem orderItem, int firstHalfYearAmount, int secondHalfYearAmount)
         {
+            orderItem.DeliveryWay = "in two parts";
             orderItem.OrderAmount = firstHalfYearAmount + secondHalfYearAmount;
 
             if (orderItem.OrderAmount > 20)
@@ -76,6 +78,7 @@ namespace DemoCSharpBasics.Shapes
         //order item processing third way - delivery at the order amount in n parts, where n is number between 3 and 12
         public OrderItem ProcessOrderItemDeliveryInNParts(OrderItem orderItem, int[] amounts)
         {
+            orderItem.DeliveryWay = $"in {amounts.Length} parts";
             orderItem.OrderAmount = 0;
             foreach (var amount in amounts)
             {
